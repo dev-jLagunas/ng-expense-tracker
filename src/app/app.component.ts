19bafd1eaccf71ed5150ef830c11a9258d84322b
Juan Lagunas <dev.jlagunas@gmail.com>
2024-09-18 12:08:38 +0900

    feat(DesktopSidebar): add basic html content and styles to component.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
 import { MobileNavbarComponent } from './navigation/mobile-navbar/mobile-navbar.component';
@@ -16,6 +16,19 @@ import { DesktopSidebarComponent } from './navigation/desktop-sidebar/desktop-si
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
-export class AppComponent {
-  title = 'expense-tracker';
+export class AppComponent implements OnInit {
+  isMobile!: boolean;
+
+  ngOnInit(): void {
+    this.checkScreenSize();
+  }
+
+  @HostListener('window:resize', ['$event'])
+  onResize(event: any): void {
+    this.checkScreenSize();
+  }
+
+  checkScreenSize(): void {
+    this.isMobile = window.innerWidth < 768;
+  }
 }
